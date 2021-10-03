eb2bd1a0f4f759385b7b8d598b041682e07d2c9c
Doan Tran Huu Phuoc <phuoc.doan0412@hcmut.edu.vn>
2021-10-03 19:07:04 +0700

    [WA-21]Navbar prototype

--- src/components/nav-bar/components/auth_guest_buttons/auth_guest_buttons.js
+++ src/components/nav-bar/components/auth_guest_buttons/auth_guest_buttons.js
@@ -4,9 +4,9 @@ import './auth_guest_buttons.css'
 export default function AuthGuestButtons() {
   return (
     <div id="sign-in-container">
-      <a id="sign-up-button" href="/login">Đăng ký</a>
-      <div id="blank">|</div>
-      <a id="login-button" href="/login">Đăng nhập</a>
+      <a className="buttons-bar" href="/login">Đăng ký</a>
+      <div className="blank">|</div>
+      <a className="buttons-bar" href="/login">Đăng nhập</a>
     </div>
   )
 }
--- src/components/nav-bar/components/header_bar/header_bar.js
+++ src/components/nav-bar/components/header_bar/header_bar.js
@@ -4,13 +4,17 @@ import './header_bar.css'
 //   BsFillCartPlusFill
 // } from 'react-icons/bs'
 import AuthGuestButtons from '../auth_guest_buttons/auth_guest_buttons'
+import MenuBar from '../menu_bar/menu_bar'
 
 export default function HeaderBar() {
   return (
     <div id="header_bar">
       <div id="logo" />
-      <AuthGuestButtons />
-      <div id="cart-button" />
+      <div id="navbar-buttons">
+        <AuthGuestButtons />
+        <div id="cart-button" />
+      </div>
+      <MenuBar />
     </div>
   )
 }
