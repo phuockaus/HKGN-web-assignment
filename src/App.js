92cc1ca0a9a664043daf02e59cdba55ece2f9076
Doan Tran Huu Phuoc <phuoc.doan0412@hcmut.edu.vn>
2021-10-03 15:55:23 +0700

    [WA-21] Components generate

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import {
+  Switch, Route, Redirect, BrowserRouter as Router
+} from 'react-router-dom'
+import Authpage from './components/auth-page'
+import Navbar from './components/nav-bar'
 
 function App() {
-  return <div>this is the beginning of app</div>
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/login" />
+        </Route>
+        <Route path="/login">
+          <Authpage />
+        </Route>
+        <Route path="/home">
+          <Navbar />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App
---
+++ src/components/nav-bar/components/auth_guest_buttons.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AuthGuestButtons() {
+  return (
+    <div>
+      <a href="/">Đăng nhập</a>
+    </div>
+  )
+}
