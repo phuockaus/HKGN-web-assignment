401a97cdfb85e813b5aa5db1687a9e9b4dcc4e80
Doan Tran Huu Phuoc <phuoc.doan0412@hcmut.edu.vn>
2021-10-04 18:45:39 +0700

    [WA-21] Add cart-icon

--- src/components/nav-bar/components/header_bar/header_bar.js
+++ src/components/nav-bar/components/header_bar/header_bar.js
@@ -1,18 +1,30 @@
 import React from 'react'
 import './header_bar.css'
 // import {
-//   BsFillCartPlusFill
-// } from 'react-icons/bs'
+//   IoAirplane
+// } from 'react-icons/io5'
+import {
+  FontAwesomeIcon
+} from '@fortawesome/react-fontawesome'
+import {
+  faCartPlus
+} from '@fortawesome/free-solid-svg-icons'
+
 import AuthGuestButtons from '../auth_guest_buttons/auth_guest_buttons'
 import MenuBar from '../menu_bar/menu_bar'
+import Logo from './assets/logo.jpg'
 
 export default function HeaderBar() {
   return (
     <div id="header_bar">
-      <div id="logo" />
+      <div id="logo">
+        <img src={Logo} alt="logo" />
+      </div>
       <div id="navbar-buttons">
         <AuthGuestButtons />
-        <div id="cart-button" />
+        <div id="cart-button">
+          <FontAwesomeIcon icon={faCartPlus} id="cart-icon" />
+        </div>
       </div>
       <MenuBar />
     </div>
