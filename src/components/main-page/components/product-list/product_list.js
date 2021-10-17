7c92b92fd7e30012c3a5288124d26da5e9d3dfa6
phuockaus <phuoc.doan0412@hcmut.edu.vn>
2021-10-17 20:59:50 +0700

    [WA-29] Render product list

--- src/components/main-page/components/product-list/product_list.js
+++ src/components/main-page/components/product-list/product_list.js
@@ -1,34 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './product_list.css'
 import ProductTemplate from '../product-template/product_template'
+import getProduct from '../../../../utils/getProduct'
 
 export default function ProductList() {
-  const products = ([
-    {
-      name: 'P1'
-    },
-    {
-      name: 'P2'
-    },
-    {
-      name: 'P3'
-    },
-    {
-      name: 'P4'
-    },
-    {
-      name: 'P5'
-    },
-    {
-      name: 'P6'
-    }
-  ])
-  const productList = products.map((product) => <ProductTemplate prop={product.name} />)
+  const [products, setProducts] = useState(false)
+  useEffect(() => {
+    getProduct()
+      .then((data) => {
+        setProducts(data)
+      })
+  }, [])
+
+  const productList = (prod) => {
+    if (prod) return products.map((product) => <ProductTemplate prop={product} />)
+    return null
+  }
+
   return (
     <div id="best-products-list">
       <div id="best-products-area-title">Sản phẩm nổi bật</div>
       <div id="best-products-items">
-        {productList}
+        {productList(products)}
       </div>
     </div>
   )
--- src/components/main-page/components/product-template/product_template.js
+++ src/components/main-page/components/product-template/product_template.js
@@ -1,12 +1,21 @@
+/* eslint-disable react/prop-types */
 import React from 'react'
 import './product_template.css'
 
-// eslint-disable-next-line react/prop-types
 export default function ProductTemplate({ prop }) {
+  const cost = `${parseInt(prop.cost, 10)} VND`
   return (
     <div className="product-template">
       <div id="product-template-content">
-        {prop}
+        <div id="product-template-image">
+          <img src={prop.image_link} alt="product_logo" />
+        </div>
+        <div id="product-template-name">
+          {prop.name}
+        </div>
+        <div id="product-template-cost">
+          {cost}
+        </div>
       </div>
     </div>
   )
