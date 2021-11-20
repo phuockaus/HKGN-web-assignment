04ea1cc51e456709dfcfc2556e78fde09676c9a2
nhatnam23012000 <nhatnam23012000@gmail.com>
2021-11-20 15:33:45 +0700

    [WA-32] add cookies when login and a draft of account info page

---
+++ src/components/account-info/index.js
@@ -0,0 +1,15 @@
+/* eslint-disable arrow-body-style */
+import React from 'react'
+import Navbar from '../nav-bar/index'
+import InfoFrame from './info-frame/index'
+
+const AccountInfo = () => {
+  return (
+    <div>
+      <Navbar />
+      <InfoFrame />
+    </div>
+  )
+}
+
+export default AccountInfo
--- src/utils/login.js
+++ src/utils/login.js
@@ -1,6 +1,11 @@
+import { encode } from 'string-encode-decode'
+import { useCookies } from 'react-cookie'
 import getAccount from './getAccount'
 
 export default function login(details) {
+  const [cookies, setCookie] = useCookies([
+    'accountID, firstName, lastName, phoneNumber, email, address, coupon, role'
+  ])
   let userpassword = ''
   getAccount(details.phone_number)
     .then((data) => {
@@ -9,6 +14,17 @@ export default function login(details) {
         if (userpassword === details.password) {
           // eslint-disable-next-line no-console
           console.log('True pass')
+          console.log(cookies.phoneNumber)
+          const expireDay = new Date()
+          expireDay.setDate(expireDay.getDate() + 3)
+          setCookie('accountID', encode(String(details.account_ID)), { expires: expireDay })
+          setCookie('firstName', encode(String(details.first_name)), { expires: expireDay })
+          setCookie('lastName', encode(String(details.last_name)), { expires: expireDay })
+          setCookie('phoneNumber', encode(String(details.phone_number)), { expires: expireDay })
+          setCookie('email', encode(String(details.email)), { expires: expireDay })
+          setCookie('address', encode(String(details.address)), { expires: expireDay })
+          setCookie('coupon', encode(String(details.coupon)), { expires: expireDay })
+          setCookie('role', encode(String(details.role)), { expires: expireDay })
         } else {
           // eslint-disable-next-line no-alert
           window.alert('Wrong password')
@@ -19,5 +35,7 @@ export default function login(details) {
       }
     })
     // eslint-disable-next-line no-console
-    .catch((err) => { console.log(err) })
+    .catch((err) => {
+      console.log(err)
+    })
 }
