9c27c97d2e181417761113c275f94e37be90b63e
phuockaus <phuoc.doan0412@hcmut.edu.vn>
2021-11-22 10:29:00 +0700

    [WA-37] Update front-end and auto redirect if not logined yet

--- src/components/payment-page/index.js
+++ src/components/payment-page/index.js
@@ -1,9 +1,12 @@
 /* eslint-disable max-len */
+import Cookies from 'js-cookie'
 import React from 'react'
+import { Redirect } from 'react-router-dom'
 import Navbar from '../nav-bar'
 import Main from './components/main/main'
 
 export default function PaymentInfo() {
+  if (!Cookies.get('accountID')) return <Redirect to="/login" />
   return (
     <div>
       <Navbar />
